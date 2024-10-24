6d1efe1cc54164fbf0d2d0fabf1c0101d99bd75c
Diego Morales <diegomatiasmoralesrojas@gmail.com>
2024-10-24 08:20:14 -0500

    first commit

---
+++ src/test/java/pe/edu/cibertec/web/EfMoralesDiegoApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EfMoralesDiegoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
